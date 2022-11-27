b5a365b1a91bed51567250690df1e126c63bfa5a
GameurDuXVI <gameur.codeur@gmail.com>
2022-11-27 13:29:43 +0100

    Adding filter enums

---
+++ app/src/main/java/be/arnaud/rocketleaguestats/api/Platform.kt
@@ -0,0 +1,17 @@
+package be.arnaud.rocketleaguestats.api
+
+import be.arnaud.rocketleaguestats.R
+
+enum class Platform(
+    val platformId: Int,
+    val displayName: String,
+    val typeName: String,
+    val drawableResource: Int
+) {
+    ALL(-1, "", "all", -1),
+    XBOX(1, "Xbox", "xbl", R.drawable.ic_xbox),
+    PLAY_STATION(2, "PlayStation", "psn", R.drawable.ic_play_station),
+    STEAM(3, "Steam", "steam", R.drawable.ic_steam),
+    EPIC_GAMES(8, "Epic Games", "epic", R.drawable.ic_epic_games),
+    SWITCH(20, "Switch", "switch", R.drawable.ic_switch)
+}
---
+++ app/src/main/java/be/arnaud/rocketleaguestats/api/PlayList.kt
@@ -0,0 +1,14 @@
+package be.arnaud.rocketleaguestats.api
+
+enum class PlayList(val id: Int) {
+    NONE(13),
+    UN_RANKED(0),
+    HOOPS(27),
+    RUMBLE(28),
+    DROP_SHOT(29),
+    SNOW_DAY(30),
+    TOURNAMENT_MATCHES(34),
+    RANKED1v1(10),
+    RANKED2v2(11),
+    RANKED3v3(13)
+}
